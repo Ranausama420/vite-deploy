14a6f9c72f6f5bfafa45a496c58dfd2969d5f5bb
Ranausama420 <43530464+Ranausama420@users.noreply.github.com>
2023-06-07 16:42:31 +0500

    aiadded

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios';
-interface FAQItem {
-  question: string;
-  answer: string[];
-  source: { page_content: string; pdf_numpages: number; source: string;}[];
+import { useState } from 'react';
+import Controller from "./components/Controller";
+import FAQs from "./components/FAQlist";
+import Radio from "./components/Options";
+import ExtractedPDF from "./components/PDFview";
+import Navbar from "./components/Navbar";
+
+enum View {
+  CHATBOT = "Chatbot",
+  FAQ_BANK = "FAQ Bank",
+  RAW_PDF = "RAW PDF",
 }
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // const [faqData, setFaqData] = useState<FAQItem[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = async () => {
-    console.log('fetchData')
-    try {
-      setIsLoading(true);
-      const response = await axios.post('https://aichatbot.herokuapp.com/readdb/', {}, {
-        headers: {
-          Accept: 'application/json',
-        },
-      });
+  const [selectedRadioButton, setSelectedRadioButton] = useState<number | null>(null);
+  const [view, setView] = useState<View>(View.CHATBOT);
 
-      // Handle the response data
-      const parsedData: FAQItem[] = [];
-      console.log(response.data);
-      const data=response.data
-      // setPdfData(response.data.msg);
-      data.forEach((item: any) => {
-        // console.log(item)
-        // console.log(typeof item)
-        const question=item.question
-        const answers=[item.answer]
-        const source=item.source
 
-      const questionExists = parsedData.some((data) => data.question === question);
-      if (questionExists) {
-        // Find the existing data with the matching question
-        const existingData = parsedData.find((data) => data.question === question);
-
-        if (existingData) {
-          // Append the new answer to the existing data's answer list
-          existingData.answer.push(...answers as string[]);
-          existingData.source = item.source;
-        }
-      } else {
-        // Add a new entry to the parsedData list
-        parsedData.push({
-          question,
-          answer: answers as string[],
-          source: source as {
-            page_content: string;
-            pdf_numpages: number;
-            source: string;
-          }[],
-        });
-      }
-
-        // setFaqData(parsedData);
-      
-  
-      });
-
-      
-    } catch (error) {
-      // Handle errors
-      console.error('Error loading data:', error);
-    } finally {
-      setIsLoading(false); // Set isLoading to false after the request is completed (whether success or error)
-    }
+  const handleNavOptionClick = (selectedView: View) => {
+    setView(selectedView);
   };
 
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + usama10</h1>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}> */}
-        <button onClick={fetchData}>
-        
-          count is {isLoading}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Navbar activeOption={view} handleNavOptionClick={handleNavOptionClick} />
+
+      {/* Main Content */}
+      {/* Main Content */}
+      {view === View.CHATBOT && (
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: '1', padding: '10px' }}>
+            <Controller radio={selectedRadioButton} />
+          </div>
+          <div style={{ flex: '1', padding: '10px' }}>
+            <div style={{ display: 'flex' }}>
+              <div className="sec1" style={{ flex: '1', paddingTop: '100px' }}>
+                <Radio selectedRadioButton={selectedRadioButton} setSelectedRadioButton={setSelectedRadioButton} />
+              </div>
+              <div className="sec1" style={{ flex: '1' }}>
+                <ExtractedPDF width={460} height={330} />
+              </div>
+            </div>
+            <div style={{ display: 'flex' }}>
+              <div className="sec2" style={{ height: '480px', width: '400px', overflowY: 'scroll', flex: '1',marginTop:'10px', scrollbarWidth: 'thin', scrollbarColor: '#ccc transparent' }}>
+                <FAQs />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {view === View.FAQ_BANK && (
+        <div>
+          {/* Add your FAQ Bank component here */}
+          <div style={{ display: 'flex' }}>
+              <div className="sec2" style={{ height: '100%', width: '400px', overflowY: 'scroll', flex: '1' }}>
+                <FAQs />
+              </div>
+              {/* <div className="sec2" style={{ height: '700px', width: '400px', overflowY: 'scroll', flex: '1', border: '1px solid #ccc' }}>
+              <ExtractedPDF text={loremIpsum} width={750} height={700} />
+              </div> */}
+            </div>
+        </div>
+      )}
+      {view === View.RAW_PDF && (
+        <div>
+          {/* Add your FAQ Bank component here */}
+          <div style={{ display: 'flex' }}>
+              <div className="sec2" style={{ height: '120%', width: '100%', overflowY: 'scroll', flex: '1', border: '1px solid #ccc',padding:'1%' }}>
+              <ExtractedPDF  width={1700} height={800} />
+              </div>
+            </div>
+        </div> 
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
